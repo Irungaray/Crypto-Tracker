5fd33d5d67b3df09f903b64c03e11536acfe72a0
Irungaray <lionelirungaray@gmail.com>
2021-02-04 17:36:20 -0300

    Created Details View

--- src/components/coins/CoinDetailScreen.js
+++ src/components/coins/CoinDetailScreen.js
@@ -1,21 +1,124 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, SectionList } from 'react-native';
+
+import Colors from '../../res/colors';
 
 class CoinDetailScreen extends Component {
 
-  componentDidMount() {
-    console.log("Coin:", this.props.route.params)
+  state = {
+    coin: {}
   }
 
+  getSymbolIcon = (coinNameId) => {
+    if (coinNameId) {
+      return `https://c1.coinlore.com/img/16x16/${coinNameId}.png`;
+    }
+  };
+
+  getSections = (coin) => {
+    const data = [
+      {
+        title: "Market Cap",
+        data: [coin.market_cap_usd]
+      },
+      {
+        title: "Volume - 24h",
+        data: [coin.volume24]
+      },
+      {
+        title: "Changes - 24h",
+        data: [coin.percent_change_24h]
+      }
+    ];
+
+    return data;
+  }
+
+  componentDidMount() {
+    const { coin } = this.props.route.params;
+
+    this.props.navigation.setOptions({ title: coin.symbol })
+
+    this.setState({ coin });
+  };
+
   render() {
+
+    const { coin } = this.state;
+
     return (
-      <View>
-        <Text>
-          Coin Detail Screen
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.subHeader}>
+          <Image
+            style={styles.iconImg}
+            source={{uri: this.getSymbolIcon(coin.nameid)}}
+          />
+
+          <Text style={styles.titleText}>{coin.name}</Text>
+        </View>
+
+        <SectionList
+          sections={this.getSections(coin)}
+
+          keyExtractor={( item ) => item}
+
+          renderItem={({ item }) =>
+            <View style={styles.sectionItem}>
+              <Text style={styles.itemText}>
+                { item }
+              </Text>
+            </View>
+          }
+
+          renderSectionHeader={({ section }) =>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionText}>
+                {section.title}
+              </Text>
+            </View>
+          }
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.charade
+  },
+  subHeader: {
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    padding: 16,
+    flexDirection: "row"
+  },
+  titleText: {
+    marginLeft: 8,
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16
+  },
+  iconImg: {
+    width: 25,
+    height: 25
+  },
+  sectionHeader : {
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    padding: 8,
+  },
+  sectionItem: {
+    padding: 8
+  },
+  itemText: {
+    color: "#fff",
+    fontSize: 14,
+  },
+  sectionText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold"
+  }
+})
 
 export default CoinDetailScreen;
