72d1d64977999d542494ce1b21365b96a08d87a5
Irungaray <lionelirungaray@gmail.com>
2021-02-04 08:44:34 -0300

    Implemented Navigation with Pressable

--- src/components/coins/CoinsScreen.js
+++ src/components/coins/CoinsScreen.js
@@ -1,16 +1,53 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 class CoinsScreen extends Component {
+
+  handlePress = () => {
+    console.log('Go to details', this.props);
+
+    this.props.navigation.navigate('CoinDetail');
+  }
+
   render() {
     return (
-      <View>
-        <Text>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
           Coins Screen
         </Text>
+
+        <Pressable onPress={this.handlePress} style={styles.btn}>
+          <Text style={styles.btnText}>
+            Go to Details
+          </Text>
+        </Pressable>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  cointainer: {
+    flex: 1,
+    backgroundColor: "grey",
+    alignItems: "center"
+  },
+
+  titleText: {
+    textAlign: "center"
+  },
+
+  btn: {
+    padding: 8,
+    backgroundColor: "black",
+    borderRadius: 8,
+    margin: 16
+  },
+
+  btnText: {
+    color: "#fff",
+    textAlign: "center"
+  }
+})
 
 export default CoinsScreen;
