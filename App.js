37c13d13890637f71e7de83c8feb77c35f86d6c1
Irungaray <lionelirungaray@gmail.com>
2021-02-07 10:10:06 -0300

    Added Tabs

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import CoinStack from './src/components/coins/CoinStack';
+
+import Colors from './src/res/colors';
+
+const Tabs = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinStack />
+      <Tabs.Navigator
+        tabBarOptions={{
+          tintColor: "#fefefe",
+          style: {
+            backgroundColor: Colors.blackPearl
+          }
+        }}
+      >
+        <Tabs.Screen
+          name="Coins"
+          component={CoinStack}
+          options={{
+            tabBarIcon: ({ size, color }) => {
+              return (
+                <Image
+                  style={{ tintColor: color, width: size, height: size }}
+                  source={require('./src/assets/bank.png')}
+                />
+              )
+            }
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
